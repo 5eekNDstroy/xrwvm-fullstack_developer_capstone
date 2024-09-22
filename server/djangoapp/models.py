5115831b56d5c148f20d037d377742f4fc146827
5eekNDstroy <66516971+5eekNDstroy@users.noreply.github.com>
2024-09-22 04:29:31 +0000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -54,17 +54,16 @@ class CarModel(models.Model):
         (COUPE, "Coupe"),
         (TRUCK, "Truck"),
     ]
-    type = models.CharField(max_length=20, 
+    type = models.CharField(max_length=20,
                             choices=CAR_TYPE_CHOICES, default="Sedan")
-    year = models.IntegerField(default=2023, 
+    year = models.IntegerField(default=2023,
                                validators=[
                                    MaxValueValidator(2023),
                                    MinValueValidator(2015)
                                    ])
-    color = models.CharField(max_length=50,
-                              blank=True, null=True)
-    price = models.DecimalField(max_digits=10,
-                                 decimal_places=2, blank=True, null=True)
+    color = models.CharField(max_length=50, blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return self.name
