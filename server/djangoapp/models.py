30891469d18729769047d591eac1acaf925a5faf
5eekNDstroy <66516971+5eekNDstroy@users.noreply.github.com>
2024-09-22 04:25:40 +0000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -54,10 +54,17 @@ class CarModel(models.Model):
         (COUPE, "Coupe"),
         (TRUCK, "Truck"),
     ]
-    type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES, default="Sedan")
-    year = models.IntegerField(default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)])
-    color = models.CharField(max_length=50, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    type = models.CharField(max_length=20, 
+                            choices=CAR_TYPE_CHOICES, default="Sedan")
+    year = models.IntegerField(default=2023, 
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                                   ])
+    color = models.CharField(max_length=50,
+                              blank=True, null=True)
+    price = models.DecimalField(max_digits=10,
+                                 decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return self.name
