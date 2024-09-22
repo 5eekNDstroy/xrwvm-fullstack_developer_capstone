56006dcfcfb3e5e2295f39fe22f68b973cdfe13e
5eekNDstroy <66516971+5eekNDstroy@users.noreply.github.com>
2024-09-22 05:28:26 +0000

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -96,7 +96,7 @@ def get_cars(request):
     return JsonResponse({"CarModels": cars})
 
 
-# Update the `get_dealerships` render list of dealerships 
+# Update the `get_dealerships` render list of dealerships
 # all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if (state == "All"):
@@ -134,7 +134,7 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
@@ -145,4 +145,4 @@ def add_review(request):
             return JsonResponse({
                 "status": 401, "message": "Error in posting review"})
     else:
-        return JsonResponse({"status": 403,"message": "Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
